06d0f3f67c6a62b024bfdfec5038d7cafcb98c0f
moritzeckardt <mori.eckardt@gmail.com>
2026-01-22 19:04:51 +0100

    work on simple backprop

---
+++ src/network.py
@@ -0,0 +1,10 @@
+import random
+import numpy as np
+
+class Network():
+    def __init__(self, sizes):
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn()]
+
